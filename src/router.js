6164a5580089e55a39e435a5345607e844b34109
timelessover <shanpaodaxia@qq.com>
2019-03-05 19:21:26 +0800

    完成歌手列表页

--- src/router.js
+++ src/router.js
@@ -13,6 +13,44 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: () => import('./views/Home.vue'),
+      children:[
+        {
+          path:"/index/songsheet",
+          // component:HotSongSheet
+        }
+      ]
+    },
+    {
+      path:'/singerlist',
+      component: () => import('./views/SIngerlist.vue'),
+      children:[
+        {
+          path:'/singerlist/singer/:id',
+          // component:() => import('./view/Singer.vue')
+        }
+      ]
+    },
+    {
+      path:'/toplists',
+      // component:() => import('./view/')
+      children:[
+        {
+          path:'/toplists/toplist/:id',
+          // component:() => import('')
+        }
+      ]
+    },
+    {
+      path:'/search',
+      // component:() => 
+    },
+    {
+      path:'/album/:id',
+      // component:() => import('')
+    },
+    {
+      path:'/user',
+      // component:() => import('')
     }
   ]
 })
